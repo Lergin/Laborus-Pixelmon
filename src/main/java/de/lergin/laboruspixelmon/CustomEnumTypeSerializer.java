0d842442fe3b838610e5c56b9f7d19a11ad035c9
Lergin <lergin@gmx.de>
2017-04-10 20:47:42 +0200

    added license of the typeserializer

--- src/main/java/de/lergin/laboruspixelmon/CustomEnumTypeSerializer.java
+++ src/main/java/de/lergin/laboruspixelmon/CustomEnumTypeSerializer.java
@@ -1,3 +1,22 @@
+/**
+ * Based on https://github.com/zml2008/configurate/blob/0b6b95818a2cf19a6d46d46c09ec7d637a17b7b9/configurate-core/src/main/java/ninja/leaping/configurate/objectmapping/serialize/TypeSerializers.java
+ * by zml and Configurate contributors
+ *
+ * modification of the EnumValueSerializer class to support non uppercase only enums by Malte Laukötter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.lergin.laboruspixelmon;
 
 import com.google.common.reflect.TypeToken;
